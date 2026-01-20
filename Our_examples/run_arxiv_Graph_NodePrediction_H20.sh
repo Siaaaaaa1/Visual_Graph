488587b03bba13d977a85d77ae1d0d5715a7664c
Siaaaaaa <1092725908@qq.com>
2026-01-20 18:30:28 +0800

    Enhance run_arxiv_Graph_NodePrediction_H20.sh: enable gradient checkpointing for improved memory efficiency during training.

--- Our_examples/run_arxiv_Graph_NodePrediction_H20.sh
+++ Our_examples/run_arxiv_Graph_NodePrediction_H20.sh
@@ -94,7 +94,7 @@ python3 -m verl.trainer.main_ppo \
     actor_rollout_ref.actor.use_kl_loss=False \
     actor_rollout_ref.actor.kl_loss_coef=0.0 \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
-    actor_rollout_ref.model.enable_gradient_checkpointing=False \
+    actor_rollout_ref.model.enable_gradient_checkpointing=True \
     actor_rollout_ref.actor.fsdp_config.param_offload=False \
     actor_rollout_ref.actor.fsdp_config.optimizer_offload=True \
     actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=32 \
